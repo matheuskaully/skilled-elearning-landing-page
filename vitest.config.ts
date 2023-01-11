fbf43becae6461e227bee0de9b543d93a16a6efe
Pedro Miguel <pedromrap@gmail.com>
2023-01-11 16:48:48 -0300

    test: criação do ambiente de testes

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+/// <reference types="vitest" />
+import { mergeConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['../.test/setup.js'],
+      include: ['**/(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      exclude: ['node_modules', 'dist', '.idea', '.git', '.cache']
+    }
+  })
+);
