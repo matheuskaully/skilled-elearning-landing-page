2cf1a73e4882fdd2b76c03ba06c55f563bd8971e
matheuskaully <kaully.santos@gmail.com>
2023-01-11 02:02:10 -0300

    add global style

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,9 +1,20 @@
 import styled from "styled-components"
 
 export const Container = styled.header`
-  background: blueviolet;
+  background: var(--purple);
 
   h1 {
     font-family: 'Plus Jakarta Sans', sans-serif;
+  }
+
+  button {
+    background: var(--gradient2);
+    padding: 100px;
+
+    :hover {
+      filter: brightness(1.1);
+      transition: 200ms;
+      cursor: pointer;
+    }
   }
 `
