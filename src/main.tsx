d6b964ee2e5a292f38afc219dc8aa87b7de6470b
Pedro Miguel <pedromrap@gmail.com>
2023-01-11 21:33:21 -0300

    chore: alteração na importação do tema

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from './styles/global'
+import { GlobalStyle } from './styles/global';
 import App from './App';
-import theme from './styles/theme';
+import { theme } from './styles/theme';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
