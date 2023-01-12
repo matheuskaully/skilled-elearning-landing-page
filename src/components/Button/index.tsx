71db5bdf49babe335d1cfdefda92ae60a1fde4dc
Pedro Miguel <pedromrap@gmail.com>
2023-01-11 21:31:49 -0300

    refactor(Button): refatoração componente button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles';
+
+interface ButtonProps {
+  children: React.ReactNode;
+}
+
+export function Button({ children }: ButtonProps) {
+  return <S.Button>{children}</S.Button>;
+}
