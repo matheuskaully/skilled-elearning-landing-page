f341f53ebe0b253c76c75b3e77a6d40dcf6ee486
Pedro Miguel <pedromrap@gmail.com>
2023-01-11 21:24:47 -0300

    style: adicionado variavies no global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,21 +1,24 @@
-import { createGlobalStyle } from "styled-components"
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  :root {
-    --purple: #666ca3;
-    --black: #13183F;
-    --gray: #83869a;
-    --pink1: #f74780;
-    --pink2: #ffa7c3;
-    --white: #fff;
-    --gradient1: linear-gradient(180deg, #ff6f48, #f02aa6);
-    --gradient2: linear-gradient(180deg, #f02aa6, #4851ff);
-  }
-
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Plus Jakarta Sans', sans-serif;
+   
   }
-`
+
+  body * {
+    font-family: ${({ theme }) => theme.font.family};
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  #root {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+`;
