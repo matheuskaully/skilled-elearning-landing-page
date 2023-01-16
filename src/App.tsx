9de558fe45d68ca7eb6d71ec5537ef42d12dc9b1
Pedro Miguel <pedromrap@gmail.com>
2023-01-16 17:11:21 -0300

    chore: alteração nos icons para a pasta public

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,5 @@
-import Header from './components/Header';
-import Card from './components/Card';
-import { data } from './main';
+import Home from './pages/Home';
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      {
-        data.map(item => {
-          return (
-            <Card 
-              title={item.title} 
-              paragraph={item.paragraph} 
-              srcIconImg={item.srcIcon} 
-            />
-          )
-        })
-      }
-    </>
-  );
+  return <Home />;
 }
